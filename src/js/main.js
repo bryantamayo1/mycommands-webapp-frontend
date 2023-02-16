2c9f8bcc382524fc5db47a4fb6e372561c6b911a
Bryan <david_bryan34@hotmail.com>
2023-02-16 18:47:45 +0000

    Building list step v1 ... Ok

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,9 @@ import '../styles/normalize.css';
 import '../styles/main.css';
 import { Services } from './services';
 
+// Local variables
+let inputValue = "";
+
 /**
  * Hnadle input’s focus search
  */
@@ -9,6 +12,7 @@ const  handleFocusInputSearch = () => {
     const input = document.getElementsByClassName("search__input")[0];
     const search__container = document.getElementsByClassName("search")[0];
     
+    // Handle style css
     input.addEventListener("focus", () => {
         search__container.style.boxShadow  = "0px 0px 10px 1px #ce04a2";
         search__container.style.outline = "1px solid #ce04a2";
@@ -24,26 +28,38 @@ const  handleFocusInputSearch = () => {
 }
 
 /**
- * Show first 20 commands by default
+ * Show 20 commands
+ * @param {string} lan 
+ * @param {number} page 
  */
-const showDefaultsCoomands = async() => {
-    const lan = "/en", page = 1;
-    const data = await Services.getCommands(lan, page);
-    // Show data in table
-    const table = document.getElementsByClassName("table")[0].childNodes[1];
-    console.log(table)
+const getCommands = async(lan, page, category, command, meaning) => {
+    // Clean data
+    const list_container = document.querySelectorAll(".list-container")
+    .forEach(item => item.remove());
+    console.log("list-container")
+    console.log(list_container);
+    // list_container?.remove();
+    console.log(list_container);
+
+    const data = await Services.getCommands(lan, page, category, command, meaning);
+    // Show data in list
+    const my_container = document.getElementsByClassName("my-container")[0];
     for(let i = 0; i< data.data.length; i++){
-        const tr = document.createElement("tr");
-        const column_1 = document.createElement("td");
-        const column_2 = document.createElement("td");
+        const div = document.createElement("div");
+        const column_1 = document.createElement("p");
+        const column_2 = document.createElement("p");
         column_1.appendChild( document.createTextNode(data.data[i].command) );
         column_2.appendChild( document.createTextNode(data.data[i].en) );
-        tr.appendChild(column_1);
-        tr.appendChild(column_2);
-        table.appendChild(tr);
+        div.appendChild(column_1);
+        div.appendChild(column_2);
+        div.classList.add("list-container");
+        my_container.appendChild(div);
     }
 }
 
+/**
+ * Handle buttons language 'es' and 'es'
+ */
 const handleButtonsLanguage = () => {
     const es = document.getElementById("es");
     const en = document.getElementById("en");
@@ -55,13 +71,31 @@ const handleButtonsLanguage = () => {
     });
 }
 
+/**
+ * Hnadle input’s value
+ */
+const handleInputSearch = () => {
+    const lan = "/en", page = 1, category = "all";
+    const input = document.getElementsByClassName("search__input")[0];
+    input.addEventListener("keydown", async(event) => {
+        // Get input's value'
+        if(event.key === "Enter"){
+            // Get commands
+            getCommands(lan, page, category, input.value);
+
+            console.log(input.value);
+        }
+    });
+}
+
 function init(){
     console.log("init----");
     console.log(window.location);
     document.addEventListener("DOMContentLoaded", () => {
         handleFocusInputSearch();
-        // showDefaultsCoomands();
+        getCommands("/en", 1);
         handleButtonsLanguage();
+        handleInputSearch();
     });
 }
 
--- src/js/services.js
+++ src/js/services.js
@@ -1,6 +1,20 @@
 export class Services{
-    static async getCommands(lan = "/en", page = 1){
-        const data = await fetch(process.env.API_URL + lan + `/?page=${page}&category=all`)
+    /**
+     * @param {string} lan 
+     * @param {number} page 
+     * @param {string} category 
+     * @param {string} command 
+     * @param {string} meaning 
+     * @returns Promise
+     */
+    static async getCommands(lan = "/en", page = 1, category = "all", command, meaning){
+        let query = "";
+        if(command){
+            query = query + "&command=" + command;
+        }else if(meaning){
+            query = query + "&meaning=" + meaning;
+        }
+        const data = await fetch(process.env.API_URL + lan + `/?page=${page}&category=${category}${query}`)
         return data.json();
     }
 }
